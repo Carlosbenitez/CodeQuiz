188488df9976675df5f98b752cd8333ce73e84dd
Carlos <carlosbenitez86753@yahoo.com>
2020-07-21 19:47:41 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//add questions that fade once completed
+
+
+//add score based on timer
+
+
+//question values
+
+
+//question answers
+
+
+//correct answers
+
+
+//{
+//title: "question",
+//choices: ["choice1", choice2", "choice3"]
+//answer: "alerts"
+//}
